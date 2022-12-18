e94b85358208b6dff8208c8873bbc264f4340edb
robertvlad <vladrob96@gmail.com>
2022-12-18 19:10:24 +0100

    base dell'esercizio con struttura

--- js/script.js
+++ js/script.js
@@ -1,12 +1,124 @@
-/*
-Consegna
-Ripetiamo l'esercizio sul carosello ma questa volta implementando l'autoplay. Non dovete rifare tutto da capo, partite da quanto già fatto. Create una nuova repo e dentro copiateci i file dell'esercizio vecchio. Quindi fate un primo push. Suddividetevi poi il lavoro in step come abbiamo sempre fatto su un file di testo quindi fate un altro push. Potete poi cominciare a lavorare sull'esercizio.
-Consigli:
-*Concentratevi sul funzionamento dell'autoplay senza preoccuparvi inizialmente dell'interazione con i pulsanti. Concentratevi su questo aspetto solo in un secondo momento.
-* Pensate prima in italiano.
-* Dividete in piccoli problemi la consegna.
-* Individuate gli elementi di cui avete bisogno per realizzare il programma.
-Bonus (da fare solo se avete terminato la parte obbligatoria):
-Implementate i pulsanti per attivare e disattivare l'autoplay.
-Buon lavoro! :grande_sorriso:
-*/
+//Creo array immagini
+const imagesArray = [
+    "01.webp",
+    "02.webp",
+    "03.webp",
+    "04.webp",
+    "05.webp"
+]
+
+//Creiamo dinamicamente i div con le immagini del carosello
+let itemsContent = '';
+let itemsThumbnails = ""
+
+for(let i = 0; i < imagesArray.length; i++){
+    itemsContent += `<div class="item">
+        <img src="./img/${imagesArray[i]}">
+    </div>`
+
+    itemsThumbnails += `<div class="thumb"><img src="./img/${imagesArray[i]}"></div>`
+}
+
+//inseriamo le immagini nel div che le deve contenere
+const itemsSlider = document.querySelector('.item-slider');
+itemsSlider.innerHTML += itemsContent;
+
+const thumbnailsPrev = document.querySelector(".thumbnails")
+thumbnailsPrev.innerHTML += itemsThumbnails
+
+//Prendiamo la prima immagine dell'array e la rendiamo attiva
+
+//const items = document.querySelector('.item'); //ALTERNATIVA
+
+const items = document.getElementsByClassName('item');
+let itemActive = 0;
+
+items[itemActive].classList.add('active');
+
+//rendo attivo anche il primo cerchio di navigazione
+
+const circles = document.getElementsByClassName('circle');
+
+circles[itemActive].classList.add('active');
+
+const thumbnails = document.getElementsByClassName("thumb")
+thumbnails[itemActive].classList.add("active")
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', function(){
+    
+    // 1 - SE l'immagine non è l'ultima da mostare:
+        // 1.1 - Togliere la classe active all'imagine attuale e metterla al successivo
+        // 1.2 - Togliere la classe active al pallino della bar di scorrimento, e metterla al successivo
+        // 1.3 - Incremento
+    // --- ALTRIMENTI non eseguo nessun cambiamento
+
+    if(itemActive < items.length - 1){
+
+        // Togliere la classe active all'imagine e del pallino attuale
+        items[itemActive].classList.remove("active");
+        circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
+
+        // Incremento
+        itemActive++;
+
+        // Aggiungere la classe active all'elemento successivo
+
+        items[itemActive].classList.add("active")
+        circles[itemActive].classList.add("active")
+        thumbnails[itemActive].classList.add("active")
+
+    }else{
+        items[itemActive].classList.remove("active");
+        circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
+
+        itemActive = 0;
+
+        items[itemActive].classList.add("active")
+        circles[itemActive].classList.add("active") 
+        thumbnails[itemActive].classList.add("active")
+    }
+});
+
+prev.addEventListener('click', function(){
+    
+    // 1 - SE l'immagine non è la prima da mostare:
+        // 1.1 - Togliere la classe active all'imagine attuale e metterla al precedente
+        // 1.2 - Togliere la classe active al pallino della bar di scorrimento, e metterla al precedente
+        // 1.3 - Decremento
+    // --- ALTRIMENTI non eseguo nessun cambiamento
+
+    if(itemActive > 0){
+
+        // Togliere la classe active all'imagine e del pallino attuale
+        items[itemActive].classList.remove("active");
+        circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
+
+        // Decremento
+        itemActive--;
+
+        // Aggiungere la classe active all'elemento precedente
+
+        items[itemActive].classList.add("active")
+        circles[itemActive].classList.add("active")
+        thumbnails[itemActive].classList.add("active")
+
+    }else{
+        items[itemActive].classList.remove("active");
+        circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
+
+        itemActive = items.length - 1;
+
+        items[itemActive].classList.add("active")
+        circles[itemActive].classList.add("active") 
+        thumbnails[itemActive].classList.add("active")
+    }
+})
+
+
